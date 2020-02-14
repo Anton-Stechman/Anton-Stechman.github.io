2819102f5fbb121ab5b6ef89c9742a2a6ceabef5
Anton-Stechman <49045849+Anton-Stechman@users.noreply.github.com>
2020-02-14 16:25:50 +1300

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,44 @@
-const rButtons=document.querySelector('.buttons-right');const lButtons=document.querySelector('.buttons-left');const rightButtons=Array.from(rButtons.children);const leftButtons=Array.from(lButtons.children);const navButtons=leftButtons.concat(rightButtons);const pageArea=document.querySelector('.page-display');var currentButton;var nextButton;const getCurrentButton=()=>{currentButton=document.querySelector('.navButton-selected');}
-const setCurButton=(nBtn)=>{getCurrentButton();if(currentButton!=null)
-{$(currentButton).removeClass('navButton-selected');}
-if(nBtn!=null)
-{$(nBtn).addClass('navButton-selected');currentButton=nBtn;}}
-const loadPage=(page,button,title)=>{$(pageArea).attr('src',page);setCurButton(button);document.title=title;}
-window.onload=()=>{}
+const rButtons      = document.querySelector('.buttons-right');
+const lButtons      = document.querySelector('.buttons-left');
+const rightButtons  = Array.from(rButtons.children);
+const leftButtons   = Array.from(lButtons.children);
+const navButtons    = leftButtons.concat(rightButtons);
+const pageArea      = document.querySelector('.page-display');
+
+var currentButton;
+var nextButton;
+
+const  getCurrentButton = () =>
+{
+    currentButton = document.querySelector('.navButton-selected');
+}
+
+const setCurButton = (nBtn) =>
+{
+    getCurrentButton();
+    
+    if (currentButton != null)
+    {
+        $(currentButton).removeClass('navButton-selected');
+    }
+    
+    if (nBtn != null)
+    {
+        $(nBtn).addClass('navButton-selected');
+        currentButton = nBtn;
+    }
+}
+
+const loadPage = (page, button, title) =>
+{
+    $(pageArea).attr('src',page);
+    setCurButton(button);
+    document.title = title;
+}
+
+
+
+window.onload = () =>
+{
+    //loadPage("test.html",false);
+}
