07c9e49052d46b6d7ed14c0b1a87847ddd9c1348
Anton-Stechman <49045849+Anton-Stechman@users.noreply.github.com>
2020-02-14 15:14:28 +1300

    Add files via upload

---
+++ javascript/pageLoader.js
@@ -0,0 +1,8 @@
+const pageArea = document.querySelector('.page-display');
+const loadingScreen = document.querySelector('.loadingScreen');
+
+function loadPage(page, button)
+{
+    $(pageArea).attr('src', page);
+    console.log(button);
+}
